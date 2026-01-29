ccac879015549c9e7b3b4079dc9bfced2f4104d6
georgek2 <george22k44@gmail.com>
2026-01-30 01:09:03 +0300

    Buy and sell actions setup

---
+++ agent/execute_contracts.py
@@ -0,0 +1,42 @@
+
+import os 
+from dotenv import load_dotenv
+import logging 
+from binance.client import Client 
+
+
+
+class ArbitrageAgent:
+
+    '''
+        Agent responsible for making buy and sell orders based on 
+        identified opportunities and potential profits
+    '''
+
+    def __init__(self):
+        load_dotenv()
+
+        api_key = os.getenv('BINANCE_API_KEY')
+        api_secret = os.getenv('BINANCE_API_SECRET')
+        self.client = Client(api_key, api_secret)
+
+        # Setting up logging for system processes
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO)
+
+    def buy(self, symbol: str, quantity: float, price: float):
+        '''
+        Buys contracts and waits for sell after price goes up
+        '''
+        self.logger.info(
+            f'Buy requested: {symbol} - {quantity} ...Price: {price}' 
+        )
+
+    def sell(self, symbol: str, quantity: float, price: float):
+        ''' 
+        Sells pair bought earlier for a reasonable profit
+        '''
+        self.logger.info(
+            f'Sell requested: {symbol} - {quantity} ...Price: {price}' 
+        )
+
--- agent/extact_transform.py
+++ agent/extact_transform.py
@@ -7,7 +7,7 @@ from binance.client import Client
 from binance import ThreadedWebsocketManager
 
 
-from .buy_sell import ArbitrageAgent
+# from .buy_sell import ArbitrageAgent
 '''
 Docstring for agent.extact_transform
 Takes care of extracting and transforming data for the agent module.
@@ -147,11 +147,11 @@ class ArbitrageExtractor:
                         if profit > 0:
                             print(f"Lag arb! Buy at {old_ask}, sell now at {cur_bid}, profit: {profit:.8f}")
                             # Buy and Sell Actions...
-                            executer = ArbitrageAgent()
+                            # executer = ArbitrageAgent()
                             
-                            execute_buy = executer.buy('BROCCOLI714USDT', quantity=1, price=old_ask_f)
-                            execute_sell = executer.sell('BROCCOLI714USDT', quantity=1, price=bid_f)
-                            print(f"Executed Buy: {execute_buy}, Executed Sell: {execute_sell}")
+                            # execute_buy = executer.buy('BROCCOLI714USDT', quantity=1, price=old_ask_f)
+                            # execute_sell = executer.sell('BROCCOLI714USDT', quantity=1, price=bid_f)
+                            # print(f"Executed Buy: {execute_buy}, Executed Sell: {execute_sell}")
                         else:
                             print(f"No lag arb: spread={profit:.8f}")
                     except ValueError:
